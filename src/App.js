ee8f9aeb69d2f608fc65c43db25d36fa44e66dd2
baran öden <baranoden@gmail.com>
2021-11-21 20:11:45 +0300

    add Api and Hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import axios from "axios"
+import React, {useState, useEffect} from "react"
 
 function App() {
-  return <div className="App">TEST02
+
+  const [coins, setCoins] = useState([])
+
+
+  useEffect(() => {
+    axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=true")
+    .then(res =>{
+      setCoins(res.data)
+      console.log(res.data)
+    })
+},[])
+
+
+
+  return <div className="App">
+
+
+
   </div>
 }
 
