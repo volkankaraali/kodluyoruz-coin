e0289338126829b1189fb3bdb56b466571181f5e
baranoden <baranoden@gmail.com>
2021-11-28 04:16:59 +0300

    fix Searchbar and CoinList

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import "./App.css";
 import React from "react";
+import Searchbar from "./components/Searchbar";
 
-import Home from "./components/Home";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Detail from "./components/Detail";
-import { CoinProvider } from "./context/CoinContext";
 
 function App() {
 
   return (
-    <CoinProvider>
-      <Router>
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/detail/:id" element={<Detail />} />
-        </Routes>
-      </Router>
-    </CoinProvider>
-
+    <Searchbar />
   );
 }
 
--- src/components/CoinList.jsx
+++ src/components/CoinList.jsx
@@ -1,26 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import { useCoin } from '../context/CoinContext'
+import React from 'react'
 
-function CoinList() {
-
-    const { searchInput, allCoins } = useCoin();
-    const [searchCoins, setSearchCoins] = useState([])
-
-    useEffect(() => {
-        let searchedCoins = allCoins.filter(item => {
-            if (item.name.toLowerCase().includes(searchInput.toLowerCase())) {
-                return item
-            } else return false
-        })
-        setSearchCoins(searchedCoins)
-    }, [allCoins, searchInput])
-
+function CoinList({
+    name,
+    price,
+    symbol,
+    marketcap,
+    volume,
+    image,
+    priceChange
+  }) {
     return (
-        <div>
-            {
-                searchInput !== "" ? searchCoins.map((item, i) => (<div key={i}>{item.name}</div>)) : false
+        <div className="coin-container">
+            <div className="coin-row">
+                <div className="coin">
+                    <img src={image} alt="crypto" />
+                    <h1>{name}</h1>
+                    <p className="coin-symbol">{symbol}</p>
+                </div>
+                <div className="coin-data">
+                    <p className="coin-price"> Price: ${price}</p>
+                    <p className="coin-volume">Market Volume: ${volume.toLocaleString()}</p>
+                    {priceChange < 0 ? (
+            <p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
+          ) : (
+            <p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
+          )}
 
-            }
+          <p className='coin-marketcap'>
+            Market Cap: ${marketcap.toLocaleString()}
+            </p>
+
+                </div>
+            </div>
         </div>
     )
 }
